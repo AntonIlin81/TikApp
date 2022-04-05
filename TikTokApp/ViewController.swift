3ee956ec625ffa88ac6811ac472b0165f88f5d1b
Антон <joker-a@mail.ru>
2022-04-05 18:21:40 +0300

    Setting up project

---
+++ TikTokApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TikTokApp
+//
+//  Created by Антон on 05.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
